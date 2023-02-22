d7e6ef1299124460e48b64e2718498a2c08e1047
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-02-22 15:16:13 -0300

    modificando html

--- projeto/app.js
+++ projeto/app.js
@@ -1,5 +1,13 @@
 angular.module('learndigest', []);
 
-angular.module('learndigest').controller('PlaygroundCtrl', function() {
+angular.module('learndigest').controller('PlaygroundCtrl', function($scope) {//CRIANDO UM MODULO JA COM CONTROLLER
+    $scope.welcome = "Welcome to my page"
 
+    $scope.resetNumber = function(){
+        $scope.number_choice = 0;
+    }
+});
+
+angular.module('learndigest').controller('InternalCtrl', function($scope) {
+    $scope.variables = "SCOPE LOCAL " + $scope.welcome
 });
